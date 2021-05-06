9eea2fa3afe1f3eddfa35c31ef0e485cbc3aa12a
Annhakobyan <82410047+Annhakobyan@users.noreply.github.com>
2021-05-06 19:35:27 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+# list1 = ["M", "na", "i", "Ke"]
+# list2 = ["y", "me", "s", "lly"]
+# print(list1[0]+list2[0] + " " + list1[1]+list2[1]+ " " + list1[2]+list2[2]+ " " +list1[3]+list2[3])
+
+
+                    #Given a Python list of numbers. Turn every item of a list into its square
+
+
+# lucky_numbers = [1, 7, 34, 8, 45]
+# for i in range(len(lucky_numbers)):     #inchia estex grvum "len" fubkcian
+#     lucky_numbers[i] *= lucky_numbers[i]
+# print(lucky_numbers)
+
+
+                        #Write a program that will calculate sum of lists members.
+
+
+# lucky_numbers = [1, 7, 34, 8, 45]
+# print(sum(lucky_numbers))
+
+# lucky_numbers = [1, 7, 34, 8, 45]
+# sum = 0
+# for i in lucky_numbers:
+#     sum += i
+#     print(sum)
+
+
+
+                         #Write a Python program to remove duplicates from a list.
+
+# lucky_numbers = [1, 7, 34, 7,45, 8,8,45]
+# new = set(lucky_numbers)
+# print(new)
+
+
+# list = [1, 3, 5, 6, 3, 5, 6, 1]
+# print(list)
+# new = []
+# for i in list:
+#     if i not in new:
+#         new.append(i)
+# print(new)
+
+
+                         #Write a Python program to get unique values from a list.
+
+
+
+
